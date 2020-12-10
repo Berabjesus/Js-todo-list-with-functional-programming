6a6806960b12ca78b4ea4c94aa40b7ed63cfc124
berabjesus <64360974+Berabjesus@users.noreply.github.com>
2020-12-11 00:02:26 +0300

    UPDATE object prototype

--- src/prototypes/objects.js
+++ src/prototypes/objects.js
@@ -1,16 +1,7 @@
-// Object.prototype = {
-//   is: function(type) {
-//     this = document.createElement(type)
-//   }
-// }
-
-// Object.prototype.is = function(type) {
-//   return document.createElement(type)
-// }
-
-Object.prototype.classes = function(classes) {
+Object.prototype = {
+  classes: function(classes) {
   classes.split(' ').map(newClass => this.classList.add(newClass))
+  }
 }
-
 
 export default Object.prototype
