beff1288efe9f7bc901b44be04594393fbd03265
berabjesus <64360974+Berabjesus@users.noreply.github.com>
2020-12-22 00:48:24 +0300

    ADD test for addCategoryModal function in modals.js

---
+++ test/modals.test.js
@@ -0,0 +1,13 @@
+import { addCategoryModal, addTaskModal } from '../src/helpers/modals'
+
+
+describe('addCategoryModal function', () => {
+  const id = 'jestTest'
+  const catModal = document.createElement('main')
+  catModal.innerHTML = addCategoryModal(id)
+  it('should return a div element `with modal fade text-dark` classes and an id of jestTest', () => {
+    expect(catModal.firstChild.nodeName == 'DIV').toBeTruthy()
+    expect(catModal.firstChild.getAttribute('class')).toBe('modal fade text-dark')
+    expect(catModal.firstChild.id).toBe(id)
+  });
+})
