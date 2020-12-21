60b75dacebb9dc87171825020dbfbb053363f350
berabjesus <64360974+Berabjesus@users.noreply.github.com>
2020-12-22 00:50:07 +0300

    ADD test for addTaskModal function in modals.js

--- test/modals.test.js
+++ test/modals.test.js
@@ -1,6 +1,5 @@
 import { addCategoryModal, addTaskModal } from '../src/helpers/modals'
 
-
 describe('addCategoryModal function', () => {
   const id = 'jestTest'
   const catModal = document.createElement('main')
@@ -9,5 +8,16 @@ describe('addCategoryModal function', () => {
     expect(catModal.firstChild.nodeName == 'DIV').toBeTruthy()
     expect(catModal.firstChild.getAttribute('class')).toBe('modal fade text-dark')
     expect(catModal.firstChild.id).toBe(id)
+  });
+})
+
+describe('addTaskModal function', () => {
+  const id = 'jestTestTask'
+  const taskModal = document.createElement('main')
+  taskModal.innerHTML = addTaskModal(id)
+  it('should return a div element `with modal fade text-dark` classes and an id of jestTest', () => {
+    expect(taskModal.firstChild.nodeName == 'DIV').toBeTruthy()
+    expect(taskModal.firstChild.getAttribute('class')).toBe('modal fade text-dark')
+    expect(taskModal.firstChild.id).toBe(id)
   });
 })
