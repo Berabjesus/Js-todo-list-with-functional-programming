0add56063f6270c60037c0f6cd916876cdfb7581
berabjesus <64360974+Berabjesus@users.noreply.github.com>
2020-12-22 01:18:30 +0300

    ADD tests for the button and div maker functions in main_module

--- test/main_module.test.js
+++ test/main_module.test.js
@@ -20,3 +20,27 @@ describe('Main Module is', () => {
     expect(div.hasOwnProperty('classes')).toBeTruthy()
   });
 });
+
+describe('Main Module isCollapsibleCategory', () => {
+  it('should be a button with `btn dropdown-toggle text-center mb-3 task-btn` classes', () => {
+    const btn = _it.isCollapsibleCategory('test', 1)
+    expect(btn.nodeName === 'BUTTON').toBeTruthy()
+    expect(btn.getAttribute('class')).toBe('btn dropdown-toggle text-center mb-3 task-btn')
+  });
+});
+
+describe('Main Module isClickableTask', () => {
+  it('should be an anchor tag with `btn btn-dark mb-1 w-100 task-btn subtasks-btn` classes', () => {
+    const btn = _it.isClickableTask('test')
+    expect(btn.nodeName === 'A').toBeTruthy()
+    expect(btn.getAttribute('class')).toBe('btn btn-dark mb-1 w-100 task-btn subtasks-btn')
+  });
+});
+
+describe('Main Module isAddButton', () => {
+  it('should be a div with `d-flex flex-column mt-auto` classes', () => {
+    const div = _it.isAddButton('test', 1)
+    expect(div.nodeName === 'DIV').toBeTruthy()
+    expect(div.getAttribute('class')).toBe('d-flex flex-column mt-auto')
+  });
+});
